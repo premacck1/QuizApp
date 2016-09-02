d1130ccb3f8730d38fe3b73f5ce3ef11b198e89a
Prem $ <premsuman8@gmail.com>
2016-09-02 14:04:40 +0530

    put link to the play store page in help fragment.

--- app/src/main/java/com/prembros/programming/quizapp/Help.java
+++ app/src/main/java/com/prembros/programming/quizapp/Help.java
@@ -1,5 +1,7 @@
 package com.prembros.programming.quizapp;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -7,6 +9,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.AnimationUtils;
+import android.widget.Toast;
 
 /*
 * Created by Prem $ on 7/22/2016.
@@ -21,6 +24,18 @@ public class Help extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         rootView = inflater.inflate(R.layout.help, container, false);
         rootView.startAnimation(AnimationUtils.loadAnimation(getContext(), R.anim.fragment_anim_in));
+        CustomTextViewBlack getPro = (CustomTextViewBlack) rootView.findViewById(R.id.get_pro);
+        getPro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/developer?id=Prem+Bros")));
+                } catch (Exception e){
+                    e.printStackTrace();
+                    Toast.makeText(getContext(), "No app found for this action!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
         isFragmentActive = true;
         return rootView;
     }
